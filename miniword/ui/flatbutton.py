936e6d24cc76f84e2f5d167a432411252e484a6f
C. Ecker <chrisecker@zoho.com>
2026-03-27 11:09:38 +0100

    A simple flat button.

---
+++ miniword/ui/flatbutton.py
@@ -0,0 +1,44 @@
+import wx
+
+class FlatButton(wx.Control):
+    def __init__(self, parent, label, size=(-1, 24)):
+        super().__init__(parent, size=size, style=wx.BORDER_NONE)
+        
+        self.SetBackgroundStyle(wx.BG_STYLE_PAINT)
+        
+        self.label = label
+        self.state = 'normal'
+        self.init_colors()
+
+        self.Bind(wx.EVT_PAINT, self.on_paint)
+        self.Bind(wx.EVT_ENTER_WINDOW, lambda e: self.set_state('hover'))
+        self.Bind(wx.EVT_LEAVE_WINDOW, lambda e: self.set_state('normal'))
+        self.Bind(wx.EVT_LEFT_DOWN,    lambda e: self.set_state('press'))
+        self.Bind(wx.EVT_LEFT_UP,      self.on_release)        
+
+    def init_colors(self):
+        bg = wx.SystemSettings.GetColour(wx.SYS_COLOUR_BTNFACE)
+        txt = wx.SystemSettings.GetColour(wx.SYS_COLOUR_WINDOWTEXT)
+        self.colors = {'normal': (bg, txt), 'hover': (bg, txt), 'press': (bg, txt)}
+
+    def set_state(self, state):
+        self.state = state
+        self.Refresh()
+
+    def on_release(self, event):
+        self.set_state('hover')
+        evt = wx.CommandEvent(wx.wxEVT_BUTTON, self.GetId())
+        self.GetEventHandler().ProcessEvent(evt)
+        
+    def on_paint(self, event):
+        dc = wx.AutoBufferedPaintDC(self)
+        bg, fg = self.colors[self.state]
+        dc.SetBackground(wx.Brush(bg))
+        dc.Clear()
+        
+        dc.SetFont(self.GetFont())
+        dc.SetTextForeground(fg)
+        w, h = self.GetSize()
+        tw, th = dc.GetTextExtent(self.label)
+        dc.DrawText(self.label, (w - tw) // 2, (h - th) // 2)
+
