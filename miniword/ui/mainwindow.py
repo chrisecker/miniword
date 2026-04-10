96697a887ce03e57b24eb89fe9e82a062053c4b0
C. Ecker <chrisecker@zoho.com>
2026-04-10 14:13:26 +0200

    Fix: Undo / Redo not working from main menu.

--- miniword/ui/mainwindow.py
+++ miniword/ui/mainwindow.py
@@ -146,6 +146,8 @@ class MainFrame(wx.Frame, ViewBase):
         edit_menu.AppendSeparator()
         edit_menu.Append(wx.ID_FIND,    "&Find && Replace…\tCtrl+F")
         bar.Append(edit_menu, "&Edit")
+        self.Bind(wx.EVT_MENU, lambda _: self.textview.undo(),  id=wx.ID_UNDO)
+        self.Bind(wx.EVT_MENU, lambda _: self.textview.redo(),  id=wx.ID_REDO)
         self.Bind(wx.EVT_MENU, lambda _: self.textview.cut(),   id=wx.ID_CUT)
         self.Bind(wx.EVT_MENU, lambda _: self.textview.copy(),  id=wx.ID_COPY)
         self.Bind(wx.EVT_MENU, lambda _: self.textview.paste(), id=wx.ID_PASTE)
